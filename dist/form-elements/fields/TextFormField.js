3e05bdf0b683dac35ac38a2274009e727270bd30
jacopo <jacopotrapani89@gmail.com>
2023-01-07 15:50:58 +0100

    is valid

--- dist/form-elements/fields/TextFormField.js
+++ dist/form-elements/fields/TextFormField.js
@@ -3,7 +3,7 @@ import { Form } from "react-bootstrap";
 import { getNestedValue } from "../utils/form-generator-utils";
 export default function TextFormField(props) {
     const { type, disable, values, errors, touched, setFieldValue, accessor, Header } = props;
-    const errorMessage = getNestedValue(accessor, errors);
+    const nestedError = getNestedValue(accessor, errors);
     const nestedTouched = getNestedValue(accessor, touched);
-    return _jsxs("div", Object.assign({ className: "filled form-group tooltip-end-top" }, { children: [_jsx(Form.Label, { children: Header }), _jsx(Form.Control, { disabled: disable, type: "text", name: accessor, placeholder: Header, value: getNestedValue(accessor, values), onChange: (e) => setFieldValue(e.target.value) }), _jsx(Form.Control.Feedback, Object.assign({ className: "font-weight-bold", type: "invalid", role: "alert", "aria-label": "from feedback" }, { children: errorMessage }))] }));
+    return _jsxs(Form.Group, { children: [_jsx(Form.Label, { children: Header }), _jsx(Form.Control, { isValid: nestedTouched && !nestedError, disabled: disable, type: "text", name: accessor, placeholder: Header, value: getNestedValue(accessor, values), onChange: (e) => setFieldValue(e.target.value) }), _jsx(Form.Control.Feedback, Object.assign({ className: "font-weight-bold", type: "invalid", role: "alert", "aria-label": "from feedback" }, { children: nestedError }))] });
 }
--- src/form-generator/form-elements/fields/TextFormField.tsx
+++ src/form-generator/form-elements/fields/TextFormField.tsx
@@ -9,19 +9,19 @@ export interface TextElementInterface extends BasicFormElementInterface{
 
 export default function TextFormField(props:TextElementInterface){
     const {type,disable, values, errors, touched,setFieldValue,accessor,Header} = props
-    const errorMessage = getNestedValue(accessor,errors)
+    const nestedError = getNestedValue(accessor,errors)
     const nestedTouched = getNestedValue(accessor,touched)
 
-    return <div className="filled form-group tooltip-end-top">
+    return <Form.Group>
         <Form.Label>{Header}</Form.Label>
-        <Form.Control disabled={disable} type="text" name={accessor} placeholder={Header} value={getNestedValue(accessor,values)} onChange={(e)=>setFieldValue(e.target.value)} />
+        <Form.Control isValid={nestedTouched && !nestedError} disabled={disable} type="text" name={accessor} placeholder={Header} value={getNestedValue(accessor,values)} onChange={(e)=>setFieldValue(e.target.value)} />
         <Form.Control.Feedback
             className="font-weight-bold"
             type="invalid"
             role="alert"
             aria-label="from feedback"
         >
-            {errorMessage}
+            {nestedError}
         </Form.Control.Feedback>
-    </div>
+    </Form.Group>
 }
